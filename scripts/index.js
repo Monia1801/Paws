a2a80d642d1439cfd86977af4ea295ec38daa0ed
monia <moniavoona18@gmail.com>
2026-01-12 16:01:05 +0530

    Render pet cards dynamically using JS object

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+import {pets} from './pets.js';
+
+const container=document.querySelector(".cards-container");
+
+pets.forEach((pet,idx)=>{
+  const card=document.createElement("div");
+  card.className=`card card${idx+1}`;
+
+  const badgeType=pet.type==="For Adoption"? "badge1" :"badge2";
+  const badgeText=pet.type;
+  const badgeClass=pet.type==="For Adoption" ? "home-icon" :"clock-icon";
+  const badgeSvg= pet.type==="For Adoption" ? `<path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />` : ` <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />`;
+
+  card.innerHTML=`
+    <div class="image-container">
+      <div class="${badgeType}">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="${badgeClass}">${badgeSvg}</svg>
+        ${badgeText}
+      </div>
+      <img src="${pet.image}" class="card-image">
+    </div>
+    <div class="card-content">
+      <h3>${pet.petName}</h3>
+      <p>${pet.breed} . ${pet.age}</p>
+      <p class="loc">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="loc-icon">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" /></svg>
+        ${pet.location.city}, ${pet.location.state}
+      </p>
+    </div>
+  `
+  container.appendChild(card);
+})
---
+++ scripts/pets.js
@@ -0,0 +1,74 @@
+export const pets = [
+  {
+    image: "https://i.pinimg.com/736x/0f/64/f2/0f64f26aadd4e706824ebc3d30ef2447.jpg",
+    petName: "Buddy",
+    breed: "Golden Retriever",
+    age: "8 months",
+    location: { city: "San Francisco", state: "CA" },
+    type: "For Adoption"
+  },
+  {
+    image: "https://i.pinimg.com/736x/f9/0c/26/f90c26a98f8f8d3e2b46876142508724.jpg",
+    petName: "Whiskers",
+    breed: "Persian Cat",
+    age: "1 year",
+    location: { city: "Los Angeles", state: "CA" },
+    type: "Temporary Care"
+  },
+  {
+    image: "https://i.pinimg.com/1200x/36/37/85/363785f62617303059801da782ec3c44.jpg",
+    petName: "Luna",
+    breed: "Labrador Retriever",
+    age: "1.5 years",
+    location: { city: "Austin", state: "TX" },
+    type: "For Adoption"
+  },
+  {
+    image: "https://i.pinimg.com/736x/09/42/4f/09424f8264175d68d4d900c9c391918b.jpg",
+    petName: "Mittens",
+    breed: "Maine Coon Cat",
+    age: "6 months",
+    location: { city: "Seattle", state: "WA" },
+    type: "Temporary Care"
+  },
+  {
+    image: "https://i.pinimg.com/736x/37/9f/71/379f71efe142983c6a881d5c7a32ca0f.jpg",
+    petName: "Max",
+    breed: "German Shepherd",
+    age: "2 years",
+    location: { city: "Chicago", state: "IL" },
+    type: "For Adoption"
+  },
+  {
+    image: "https://i.pinimg.com/736x/88/61/00/886100882c20c56e44831f077f56d34f.jpg",
+    petName: "Bella",
+    breed: "Indie Cat",
+    age: "9 months",
+    location: { city: "New York", state: "NY" },
+    type: "Temporary Care"
+  },
+  {
+    image: "https://i.pinimg.com/736x/b4/e1/b8/b4e1b80afa86d1847b3cbe06f8715f19.jpg",
+    petName: "Rocky",
+    breed: "Rottweiler",
+    age: "4 years",
+    location: { city: "Dallas", state: "TX" },
+    type: "For Adoption"
+  },
+  {
+    image: "https://i.pinimg.com/1200x/af/6b/98/af6b988f0bb45823b3e1a60a8e9d3d83.jpg",
+    petName: "Shadow",
+    breed: "Bombay Cat",
+    age: "3 years",
+    location: { city: "San Jose", state: "CA" },
+    type: "Temporary Care"
+  },
+  {
+    image: "https://i.pinimg.com/736x/0c/4b/2c/0c4b2c2165bd0c892dfeb4eda1f54cc8.jpg",
+    petName: "Lucy",
+    breed: "Beagle",
+    age: "2 years",
+    location: { city: "Bengaluru", state: "KA" },
+    type: "For Adoption"
+  }
+];
